b44b323e71e00fd66bfb00f01348ae0f06fca9de
luisnisc <luisnisc30mayo2005@gmail.com>
2024-10-15 11:02:10 +0200

    .

---
+++ js/card_animation.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mousemove', (e) => {
+        const rect = card.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+        const rotateX = (centerY - y) / 10; // Ajustar para que se incline correctamente
+        const rotateY = (x - centerX) / 10; // Ajustar para que se incline correctamente
+
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    });
+});
