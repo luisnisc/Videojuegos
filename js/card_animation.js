174be5d8229466bf1940de03267022a6dd32b6af
luisnisc <luisnisc30mayo2005@gmail.com>
2024-11-27 10:51:15 +0100

    mejora en el diseño responsive de la aplicación

--- js/card_animation.js
+++ js/card_animation.js
@@ -1,15 +1,21 @@
 document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('mousemove', (e) => {
-        const rect = card.getBoundingClientRect();
-        const x = e.clientX - rect.left;
-        const y = e.clientY - rect.top;
-        const centerX = rect.width / 2;
-        const centerY = rect.height / 2;
-        const rotateX = (centerY - y) / 10; // Ajustar para que se incline correctamente
-        const rotateY = (x - centerX) / 10; // Ajustar para que se incline correctamente
+    let requestId = null;
 
-        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        
+    card.addEventListener('mousemove', (e) => {
+        if (requestId) return;
+
+        requestId = requestAnimationFrame(() => {
+            const rect = card.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+            const centerX = rect.width / 2;
+            const centerY = rect.height / 2;
+            const rotateX = (centerY - y) / 10;
+            const rotateY = (x - centerX) / 10;
+
+            card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+            requestId = null;
+        });
     });
 
     card.addEventListener('mouseleave', () => {
